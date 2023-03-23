5fd0b4c642ab744029bc448c603d8a6f1207ade1
Kseniya Volchitsa <vol4ica13131313@gmail.com>
2023-03-23 15:50:54 +0300

    priority random at work

--- OOP/PriorityRandom/src/Main.java
+++ OOP/PriorityRandom/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] strings = new String[]{"a","b","c"};
+        int[] number = new int[]{1,2,10};
+    ReturnRandomElement ret1 = new ReturnRandomElement(strings, number);
+    for (int i = 0; i < 13; i++){
+        System.out.println(ret1.randomElement());
+    }
     }
 }
---
+++ OOP/PriorityRandom/src/ReturnRandomElement.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class ReturnRandomElement {
+
+    private Object[] values;
+    private int[] priorities;
+
+    private int sum;
+    private int[] weights;
+
+
+    public ReturnRandomElement(Object[] values, int[] priorities){
+        this.values = values;
+        this.priorities = priorities;
+        sum = 0;
+        weights = new int[priorities.length];
+        int j = 0;
+        for (int i : priorities){
+            sum += i;
+            weights[j] = sum-1;
+            j++;
+        }
+    }
+
+    public Object randomElement(){
+        Object result = null;
+        Random random = new Random();
+        int i = random.nextInt(sum);
+        System.out.print(i);
+        for (int j = 0; j < weights.length; j++){
+            if (i == weights[j]){
+                result = values[j];
+                break;
+            } else if (i < weights[j]){
+                result = values[j];
+                break;
+            }
+        }
+
+        return result;
+    }
+}
